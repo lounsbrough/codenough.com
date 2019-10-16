a672790f715ee818ead21fbe7bdfdea6d39a5673
David Lounsbrough <david.lounsbrough@gmail.com>
2019-10-15 23:11:10 -0500

    Adding main logo and starting to organize scss

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React from 'react';
-import LetterC from './logo/letters/LetterC';
-import LetterO from './logo/letters/LetterO';
-import LetterD from './logo/letters/LetterD';
-import LetterE from './logo/letters/LetterE';
-import LetterN from './logo/letters/LetterN';
-import LetterU from './logo/letters/LetterU';
-import LetterG from './logo/letters/LetterG';
-import LetterH from './logo/letters/LetterH';
-import './App.css';
+
+import Logo from './components/logo/Logo';
+import Letters from './components/logo/Letters';
+import './css/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="logo">
-          <LetterC className="letter letter-c"></LetterC>
-          <br></br>
-          <LetterO className="letter letter-o-1"></LetterO>
-          <br></br>
-          <LetterD className="letter letter-d"></LetterD>
-          <br></br>
-          <LetterE className="letter letter-e"></LetterE>
-          <br></br>
-          <LetterN className="letter letter-n"></LetterN>
-          <br></br>
-          <LetterO className="letter letter-o-2"></LetterO>
-          <br></br>
-          <LetterU className="letter letter-u"></LetterU>
-          <br></br>
-          <LetterG className="letter letter-g"></LetterG>
-          <br></br>
-          <LetterH className="letter letter-h"></LetterH>
-        </div>
+    <div className="app">
+      <header className="app-header colorful-background">
+        <Logo format="fillAndOutlineWhite" height="500" width="80%" />
+        <Letters />
       </header>
     </div>
   );
---
+++ src/components/logo/Letters.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import LetterC from './letters/LetterC';
+import LetterO from './letters/LetterO';
+import LetterD from './letters/LetterD';
+import LetterE from './letters/LetterE';
+import LetterN from './letters/LetterN';
+import LetterU from './letters/LetterU';
+import LetterG from './letters/LetterG';
+import LetterH from './letters/LetterH';
+
+function Letters() {
+  return (
+    <div className="logo-letters">
+        <LetterC className="letter letter-c"></LetterC>
+        <br></br>
+        <LetterO className="letter letter-o-1"></LetterO>
+        <br></br>
+        <LetterD className="letter letter-d"></LetterD>
+        <br></br>
+        <LetterE className="letter letter-e"></LetterE>
+        <br></br>
+        <LetterN className="letter letter-n"></LetterN>
+        <br></br>
+        <LetterO className="letter letter-o-2"></LetterO>
+        <br></br>
+        <LetterU className="letter letter-u"></LetterU>
+        <br></br>
+        <LetterG className="letter letter-g"></LetterG>
+        <br></br>
+        <LetterH className="letter letter-h"></LetterH>
+    </div>
+  );
+}
+
+export default Letters;
