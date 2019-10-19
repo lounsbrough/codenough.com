a29d563ff33602c2b09ab90d193a0527fd51182d
lounsbrough <lounsbrough@users.noreply.github.com>
2019-10-18 21:31:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="app">
       <header className="app-header colorful-background">
-        <Logo format="fillAndOutlineWhite" height="500" width="80%" />
+        <Logo className="logo" format="fillAndOutlineWhite" height="500" width="80%" />
         <Letters />
       </header>
     </div>
