638640e7e454fdb506cf0b401319a6f5b4774f1a
David Lounsbrough <david.lounsbrough@gmail.com>
2019-10-26 12:05:40 -0500

    Adding portrait rotation on touch

--- src/App.js
+++ src/App.js
@@ -4,15 +4,49 @@ import Logo from './components/logo/Logo';
 import Letters from './components/logo/Letters';
 import './css/App.scss';
 
-function App() {
-  return (
-    <div className="app">
-      <header className="app-header colorful-background">
-        <Logo className="logo" format="fillAndOutlineWhite" height="500" width="80%" />
-        <Letters />
-      </header>
-    </div>
-  );
+const touchHandler = () => {
+  document.querySelector('body').classList.toggle('rotation-enabled');
+};
+
+const orientationHandler = (e) => {
+  var orientation = window.screen.msOrientation || window.screen.mozOrientation || (window.screen.orientation || {}).type;
+
+  let rotatableBody;
+
+  if (orientation === 'portrait-primary') {
+    rotatableBody = document.querySelector(".rotation-enabled");
+  }
+
+  if (!rotatableBody) {
+    document.querySelector("body").style.transform = "";
+    return;
+  }
+
+  rotatableBody.style.transform = `
+    rotateZ(${e.alpha}deg)
+    rotateX(${e.beta + 270}deg)
+    rotateY(${-e.gamma}deg)`;
+};
+
+class App extends React.Component {
+  componentDidMount() {
+    if (window.DeviceOrientationEvent !== undefined) {
+      window.addEventListener("deviceorientation", orientationHandler, true);
+    }
+
+    window.addEventListener("touchstart", touchHandler);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <Logo className="logo" format="fillAndOutlineWhite" height="500" width="80%" />
+          <Letters />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
