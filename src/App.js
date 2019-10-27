8efb624098d0763e643e1731736f622da058e78d
David Lounsbrough <david.lounsbrough@gmail.com>
2019-10-27 13:38:26 -0500

    Cleaning up svg paths

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="app">
       <header className="app-header colorful-background">
-        <Logo className="logo" format="fillAndOutlineWhite" height="500" width="80%" />
+        <Logo className="logo" format="fillWhite" height="500" width="80%" />
         <Letters />
       </header>
     </div>
--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,12 +5,10 @@ const Logo = props => {
         return (
             <svg width={230} height={210} viewBox="0 0 60.854 55.563" {...props}>
                 <g paintOrder="markers stroke fill">
-                <path
-                    d="M39.687 42.333h21.167v13.23H39.687zM21.167 21.167h39.687v13.229H21.167z"
-                    fill="#1ccbd2"
-                />
-                <path d="M0 0v55.563h31.75v-13.23H13.23V13.23h18.52V0z" fill="#006680" />
-                <path d="M39.688 0h21.166v13.23H39.687z" fill="#1ccbd2" />
+                    <path d="M0 0v55.563h31.75v-13.23H13.23V13.23h18.52V0z" fill="#006680" />
+                    <path d="M39.688 0h21.166v13.23H39.687z" fill="#1ccbd2" />
+                    <path d="M21.167 21.167h39.687v13.229H21.167z" fill="#1ccbd2" />
+                    <path d="M39.687 42.333h21.167v13.23H39.687z" fill="#1ccbd2" />
                 </g>
             </svg>
         );
@@ -18,34 +16,27 @@ const Logo = props => {
         return (
             <svg width={233.024} height={213.024} viewBox="0 0 61.654 56.362" {...props}>
                 <g
-                stroke="#006680"
-                strokeWidth={0.8}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                paintOrder="markers stroke fill"
+                    stroke="#006680"
+                    strokeWidth={0.8}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    paintOrder="markers stroke fill"
                 >
-                <path
-                    d="M40.087 42.733h21.167v13.23H40.087zM21.567 21.567h39.687v13.229H21.567z"
-                    fill="#1ccbd2"
-                />
-                <path
-                    d="M.4.4v55.563h31.75v-13.23H13.63V13.63h18.52V.4z"
-                    fill="#006680"
-                />
-                <path d="M40.088.4h21.166v13.23H40.087z" fill="#1ccbd2" />
+                    <path d="M.4.4v55.563h31.75v-13.23H13.63V13.63h18.52V.4z" fill="#006680" />
+                    <path d="M40.088.4h21.166v13.23H40.087z" fill="#1ccbd2" />
+                    <path d="M21.567 21.567h39.687v13.229H21.567z" fill="#1ccbd2" />
+                    <path d="M40.087 42.733h21.167v13.23H40.087z" fill="#1ccbd2" />
                 </g>
             </svg>
         );
-    } else if (props.format === 'fillAndOutlineWhite') {
+    } else if (props.format === 'fillWhite') {
         return (
             <svg width={230} height={210} viewBox="0 0 60.854 55.563" {...props}>
                 <g paintOrder="markers stroke fill">
-                <path
-                    d="M39.687 42.333h21.167v13.23H39.687zM21.167 21.167h39.687v13.229H21.167z"
-                    fill="#fdffff"
-                />
-                <path d="M0 0v55.563h31.75v-13.23H13.23V13.23h18.52V0z" fill="#006680" />
-                <path d="M39.688 0h21.166v13.23H39.687z" fill="#fdffff" />
+                    <path d="M0 0v55.563h31.75v-13.23H13.23V13.23h18.52V0z" fill="#006680" />
+                    <path d="M39.688 0h21.166v13.23H39.687z" fill="#ffffff" />
+                    <path d="M21.167 21.167h39.687v13.229H21.167z" fill="#ffffff" />
+                    <path d="M39.687 42.333h21.167v13.23H39.687z" fill="#ffffff" />
                 </g>
             </svg>
         );
