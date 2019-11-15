48888047330c77d07f7b13b1d75782139a7fa5a4
David Lounsbrough <david.lounsbrough@gmail.com>
2019-11-14 22:01:10 -0600

    Randomize order of proficiencies

--- src/components/Proficiencies.js
+++ src/components/Proficiencies.js
@@ -2,26 +2,37 @@ import React from 'react';
 
 import proficiencies from '../data/proficiencies';
 
-const Proficiencies = () => {
-  return (
-    <div>
-        <h1>Things I have experience with</h1>
-        <br />
-        {proficiencies.map(proficiency => {
-            const SvgComponent = require(`./proficiency-icons/${proficiency.componentName}`).default;
+const randomizeOrder = (array) => {
+    return array.map((element) => ({
+        ...element,
+        order: Math.random()
+    })).sort((a, b) => a.order > b.order ? 1 : -1);
+};
 
-            return (
-                <span key={`span1-${proficiency.title}`} style={{padding: '20px', lineHeight: '100px'}}>
-                    <SvgComponent
-                        key={proficiency.title}
-                        className="proficiency-icon"
-                        fill={proficiency.color}
-                    />
-                </span>
-            );
-        })}
-    </div>
-  );
+const randomProficiencies = randomizeOrder(proficiencies);
+
+const Proficiencies = () => {
+
+
+    return (
+        <div>
+            <h1>Things I have experience with</h1>
+            <br />
+            {randomProficiencies.map(proficiency => {
+                const SvgComponent = require(`./proficiency-icons/${proficiency.componentName}`).default;
+
+                return (
+                    <span key={`span1-${proficiency.title}`} style={{padding: '20px', lineHeight: '100px'}}>
+                        <SvgComponent
+                            key={proficiency.title}
+                            className="proficiency-icon"
+                            fill={proficiency.color}
+                        />
+                    </span>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Proficiencies;
