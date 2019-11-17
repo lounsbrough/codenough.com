5406873d549364a3d1d68a3e430c690a74766ec7
David Lounsbrough <david.lounsbrough@gmail.com>
2019-11-16 21:27:04 -0600

    Show title after a few seconds

--- src/components/Proficiencies.js
+++ src/components/Proficiencies.js
@@ -28,6 +28,7 @@ const Proficiencies = () => {
                         <span
                             className='icon-span'
                             key={`icon-span-${proficiency.componentName}`}
+                            data-tooltip={proficiency.title}
                         >
                             <SvgComponent
                                 key={`icon-${proficiency.componentName}`}
