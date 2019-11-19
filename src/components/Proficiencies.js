028d5b864d2e6f81f8964a510fe57eab2a37652f
David Lounsbrough <david.lounsbrough@gmail.com>
2019-11-18 21:35:52 -0600

    I don't believe I need both click handlers

--- src/components/Proficiencies.js
+++ src/components/Proficiencies.js
@@ -28,7 +28,6 @@ const Proficiencies = () => {
                         <span
                             className='icon-span'
                             key={`icon-span-${proficiency.componentName}`}
-                            onClick={() => null}
                         >
                             <SvgComponent
                                 key={`icon-${proficiency.componentName}`}
