e8f9b6b47fcf2c932f01477ce96e59d4a7676978
David Lounsbrough <david.lounsbrough@gmail.com>
2020-03-20 17:55:14 -0500

    Adding progress bar

--- src/components/SkydivingGame.js
+++ src/components/SkydivingGame.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import Unity, { UnityContent } from 'react-unity-webgl';
+import {Progress} from 'reactstrap';
+import Unity, {UnityContent} from 'react-unity-webgl';
 
-const unityContent = new UnityContent(
-    'skydiving-game/Build/skydiving-game.json',
-    'skydiving-game/Build/UnityLoader.js'
-);
+class SkydivingGame extends React.Component {
+    constructor(props) {
+        super(props);
 
-const SkydivingGame = () => {
-    return <Unity unityContent={unityContent} />;
-};
+        this.state = {
+            loadingProgress: 0,
+            isLoading: true
+        };
+
+        this.unityContent = new UnityContent(
+            'skydiving-game/Build/skydiving-game.json',
+            'skydiving-game/Build/UnityLoader.js'
+        );
+
+        this.unityContent.on("progress", progress => {
+            this.setState({
+                loadingProgress: progress * 100
+            });
+        });
+
+        this.unityContent.on("loaded", () => {
+            this.setState({
+                isLoading: false
+            });
+        });
+    }
+
+    render() {
+        return (
+            <>
+                {this.state.isLoading && <Progress value={this.state.loadingProgress} />}
+                <Unity unityContent={this.unityContent} />
+            </>
+        );
+    }
+}
 
 export default SkydivingGame;
