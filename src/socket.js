0f1ff694c22068eb15307a0bd54cfcb93a03260e
David Lounsbrough <dlounsbrough@hy-vee.com>
2021-06-09 22:30:25 -0500

    Point to lounsbrough.com

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-export const socket = io('http://localhost:3001');
+export const socket = io('https://lounsbrough.com/stranger-things');
 
 export default React.createContext(socket);
