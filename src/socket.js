e75c65b89f6e751555518f814fc99b15e1d99c8d
David Lounsbrough <dlounsbrough@hy-vee.com>
2021-06-09 23:15:40 -0500

    Update socket path

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-export const socket = io('https://lounsbrough.com/stranger-things');
+export const socket = io('https://lounsbrough.com/', {path: '/stranger-things/socket.io'});
 
 export default React.createContext(socket);
