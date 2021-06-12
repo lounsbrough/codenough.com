84fa518f4c728333da49809282f328d142e4461b
David Lounsbrough <david.lounsbrough@gmail.com>
2021-06-11 22:23:02 -0500

    Remove useless props spread

--- src/components/stranger-things/LightsWallSvg.js
+++ src/components/stranger-things/LightsWallSvg.js
@@ -15,7 +15,6 @@ const LightsWallSvg = (props) => {
         <svg
             viewBox="0 0 210 100"
             xmlns="http://www.w3.org/2000/svg"
-            {...props}
         >
             <defs>
                 <filter
